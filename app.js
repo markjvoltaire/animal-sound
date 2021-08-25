7ad5449683b0975e8f77b07a858b930220d67144
Mark Voltaire <markjvoltaire@gmail.com>
2021-08-25 19:19:52 -0400

    sound added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // import functions
 
 // reference needed DOM elements
-
-
-// set event listeners 
+const catImage = document.getElementById('cat-image') ;
+console.log(catImage);
+// set event listeners
+const catAudio = document.getElementById('cat-audio') ;
+console.log(catAudio);
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+catImage.addEventListener('click', () => {
+    catAudio.play();
+});
